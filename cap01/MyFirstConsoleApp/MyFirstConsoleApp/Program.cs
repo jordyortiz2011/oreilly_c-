44994390451e3862582ebcc6c242a1ff9a12254a
Jordy Ortiz <desarrollador328_usi@minedu.gob.pe>
2025-01-02 16:02:49 -0500

    Operaciones

--- cap01/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ cap01/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -7,6 +7,38 @@ namespace MyFirstConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+   
+            OperatorExamples();
+        }
+
+        private static void OperatorExamples()
+        {
+
+            // This statement declares a variable and sets it to 3
+            int width = 3;
+
+            
+
+            // The ++ operator increments a variable (adds 1 to it)
+            width++;
+            // Declare two more int variables to hold numbers and
+            // use the + and * operators to add and multiply values
+            int height = 2 + 4;
+            int area = width * height;
+
+            Console.WriteLine(area);
+
+            // The next two statements declare string variables
+            // and use + to concatenate them (join them together)
+            string result = "The area";
+            result = result + " is " + area;
+            Console.WriteLine(result);
+
+
+            // A Boolean variable is either true or false
+            bool truthValue = true;
+            Console.WriteLine(truthValue);
+
         }
     }
 }
